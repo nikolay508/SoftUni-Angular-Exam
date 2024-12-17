1fce4877cf4434ccb53b08a7aac533155b31e350
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-12-18 00:57:39 +0200

    Update app.component.ts

--- the project itself/src/app/app.component.ts
+++ the project itself/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './core/navigation-bar/navigation-bar.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { HttpClient } from '@angular/common/http';
+import { EditComponent } from './shared/edit/edit.component';
+import { DetailsComponent } from './shared/details/details.component';
+import { AuthenticateComponent } from './authenticate/authenticate/authenticate.component';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +14,13 @@ import { FooterComponent } from './core/footer/footer.component';
     RouterOutlet,
     NavigationBarComponent,
     FooterComponent,
+    EditComponent,
+    DetailsComponent,
+    AuthenticateComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'project-angular';
+  title = 'project-app';
 }
