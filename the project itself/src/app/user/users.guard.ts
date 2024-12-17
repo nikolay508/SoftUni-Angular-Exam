982d54220d053e1d0cf1acc3aedfeb0eb02ab75f
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-12-18 01:00:50 +0200

    Create users.guard.ts

---
+++ the project itself/src/app/user/users.guard.ts
@@ -0,0 +1,12 @@
+import { inject } from "@angular/core";
+import { CanActivateFn, Router } from "@angular/router";
+
+export const AuthGuard: CanActivateFn = (route, state) => {
+    const router = inject(Router);
+    const isLoggedIn = true;
+
+    if (!isLoggedIn) {
+        router.navigate(['/error']);
+    }
+    return isLoggedIn;
+}
