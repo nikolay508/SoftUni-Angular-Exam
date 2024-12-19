6c10cd11175e6ac49f71514bfc7119a1f9dcecd6
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-12-19 14:33:24 +0200

    Add files via upload

--- the project itself/src/app/utils/email.validator.ts
+++ the project itself/src/app/utils/email.validator.ts
@@ -1,11 +1,11 @@
-import { ValidatorFn } from "@angular/forms";
+import { ValidatorFn } from '@angular/forms';
 
 export function emailValidator(domains: string[]): ValidatorFn {
-    const domainStr = domains.join('|');
-    const regExp = new RegExp(`[A-Za-z0-9.]{3,}@[a-z]+.(${domainStr})`);
+  const domainStr = domains.join('|');
+  const regExp = new RegExp(`[A-Za-z0-9.]{3,}@[a-z]+.(${domainStr})`);
 
-    return (control) => {
-        const isInvalid = control.value === '' || regExp.test(control.value);
-        return isInvalid ? null : { emailValidator: true };
-    }
+  return (control) => {
+    const isInvalid = control.value === '' || regExp.test(control.value);
+    return isInvalid ? null : { emailValidator: true };
+  };
 }
