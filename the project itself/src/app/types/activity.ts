2b17726e2ade09318f218349e25a8d664133ef5c
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-12-19 14:32:33 +0200

    Add files via upload

---
+++ the project itself/src/app/types/activity.ts
@@ -0,0 +1,11 @@
+export interface Activity {
+  _id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  startDate: string;
+  price: string;
+  signedOut: string[];
+  _ownerId: string;
+  __v: number;
+}
--- the project itself/src/app/types/user.ts
+++ the project itself/src/app/types/user.ts
@@ -1,22 +1,22 @@
 export interface User {
-    courses: string[];
-    _id: string,
-    email: string,
-    username: string,
-    password: string,
-    created_at: string;
-    updatedAt: string;
-    __v: number;
+  courses: string[];
+  _id: string;
+  email: string;
+  username: string;
+  password: string;
+  created_at: string;
+  updatedAt: string;
+  __v: number;
 }
 export interface UserForAuth {
-    username: string;
-    email: string;
-    password: string;
-    _id: string;
-  }
-  
-  export interface ProfileDetails {
-    username: string;
-    email: string;
-    _id: string;
-  }
+  username: string;
+  email: string;
+  password: string;
+  _id: string;
+}
+
+export interface ProfileDetails {
+  username: string;
+  email: string;
+  _id: string;
+}
